16631b0467395083c05db8e171688c50e9cfffdb
cokeboL <165128167@qq.com>
2018-06-05 22:49:51 +0800

    1

---
+++ threeSum.go
@@ -0,0 +1,65 @@
+//https://leetcode-cn.com/problems/3sum/description/
+
+func threeSum(nums []int) [][]int {
+	ret := [][]int{}
+
+	newnums := []int{}
+	flag := false
+	nummap := map[int]map[int]bool{}
+	for i, v := range nums {
+		if _, ok := nummap[v]; !ok {
+			nummap[v] = map[int]bool{}
+		}
+		if len(nummap[v]) < 3 {
+			nummap[v][i] = true
+			newnums = append(newnums, v)
+		} else {
+			flag = true
+		}
+	}
+	if flag {
+		nums = newnums
+		nummap = map[int]map[int]bool{}
+		for i, v := range nums {
+			if _, ok := nummap[v]; !ok {
+				nummap[v] = map[int]bool{}
+			}
+			nummap[v][i] = true
+		}
+	}
+	sum := 0
+	hash := map[[3]int]bool{}
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if i != j {
+				sum = nums[i] + nums[j]
+				n := 0
+				if v, ok := nummap[-sum]; ok {
+					if v[i] {
+						n++
+					}
+					if v[j] {
+						n++
+					}
+					if len(v) > n {
+						tmp := [3]int{-sum, nums[i], nums[j]}
+						if tmp[0] > tmp[1] {
+							tmp[0], tmp[1] = tmp[1], tmp[0]
+						}
+						if tmp[1] > tmp[2] {
+							tmp[1], tmp[2] = tmp[2], tmp[1]
+							if tmp[0] > tmp[1] {
+								tmp[0], tmp[1] = tmp[1], tmp[0]
+							}
+						}
+						if _, ok := hash[tmp]; !ok {
+							ret = append(ret, tmp[:])
+							hash[tmp] = true
+						}
+					}
+				}
+			}
+		}
+	}
+	return ret
+}
